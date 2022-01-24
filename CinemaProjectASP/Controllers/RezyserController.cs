d3736a7aa4f7e53cfa139c210d54506b0909d1e0
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-24 13:56:35 +0100

    rezyser controller

---
+++ CinemaProjectASP/Controllers/RezyserController.cs
@@ -0,0 +1,24 @@
+﻿using CinemaProjectASP.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaProjectASP.Controllers
+{
+    public class RezyserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RezyserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allRezyserzy = await _context.Rezyserzy.ToListAsync();
+            return View();
+        }
+    }
+}
