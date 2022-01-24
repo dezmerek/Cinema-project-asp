2547e7f6f6d29fe79abcd3b6cb049505d65e179a
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-24 14:04:17 +0100

    controller sale

---
+++ CinemaProjectASP/Controllers/SalaController.cs
@@ -0,0 +1,23 @@
+﻿using CinemaProjectASP.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CinemaProjectASP.Controllers
+{
+    public class SalaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SalaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allSale = await _context.Sale.ToListAsync();
+            return View();
+        }
+    }
+}
