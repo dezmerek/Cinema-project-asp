2932bf30cfc0997cb00a9b52641098bd3c99fd07
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-24 12:34:25 +0100

    dodanie DbContext

---
+++ CinemaProjectASP/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CinemaProjectASP.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+    }
+}
