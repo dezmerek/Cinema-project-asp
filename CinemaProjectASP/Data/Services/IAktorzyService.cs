d3d63e13b0b6fd236ccde8609b21e951fb4cbaff
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-24 16:29:31 +0100

    add interfejs

---
+++ CinemaProjectASP/Data/Services/IAktorzyService.cs
@@ -0,0 +1,14 @@
+﻿using CinemaProjectASP.Models;
+using System.Collections.Generic;
+
+namespace CinemaProjectASP.Data.Services
+{
+    public interface IAktorzyService
+    {
+        IEnumerable<Aktor> GetAll();
+        Aktor GetById(int id);
+        void Add(Aktor aktor);
+        Aktor Update(int id, Aktor newAktor);
+        void Delete(int id);
+    }
+}
