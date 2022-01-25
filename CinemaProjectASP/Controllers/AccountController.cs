b1accf373f476a7d0723a044b71df92852e12408
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-25 19:54:59 +0100

    dodanie kontrolera account

---
+++ CinemaProjectASP/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using CinemaProjectASP.Data;
+using CinemaProjectASP.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaProjectASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
