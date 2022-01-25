f71533c8d9a5b2aee64071f909ccfe4186957fc8
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-25 17:48:40 +0100

    user migracja

---
+++ CinemaProjectASP/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaProjectASP.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Display(Name="Imie i Nazwisko")]
+        public string ImieNazwisko { get; set; }
+    }
+}
