bc764ffeb9197efd6fcc04168f0859ca1befc246
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-25 01:05:58 +0100

    dodawanie rezysera

--- CinemaProjectASP/Controllers/RezyserzyController.cs
+++ CinemaProjectASP/Controllers/RezyserzyController.cs
@@ -1,5 +1,6 @@
 ﻿using CinemaProjectASP.Data;
 using CinemaProjectASP.Data.Services;
+using CinemaProjectASP.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -22,12 +23,27 @@ namespace CinemaProjectASP.Controllers
             return View(allRezyserzy);
         }
 
-        //GET: Rezyser/Szczegoly/1
+        //GET: Rezyserzy/Szczegoly/1
         public async Task<IActionResult> Szczegoly(int id)
         {
             var rezyserSzczegoly = await _service.GetByIdAsync(id);
             if (rezyserSzczegoly == null) return View("NotFound");
             return View(rezyserSzczegoly);
+        }
+
+        //GET: Rezyserzy/Dodaj
+        public IActionResult Dodaj()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Dodaj([Bind("ImieNazwisko, RokUrodzenia, Biografia")]Rezyser rezyser)
+        {
+            if(!ModelState.IsValid) return View(rezyser);
+
+            await _service.AddAsync(rezyser);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
