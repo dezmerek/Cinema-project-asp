5f0593541a42da5197ed46c3038233c092bde7fd
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-25 20:23:34 +0100

    wyglad logowania

---
+++ CinemaProjectASP/Controllers/KontoController.cs
@@ -0,0 +1,50 @@
+﻿using CinemaProjectASP.Data;
+using CinemaProjectASP.Models;
+using CinemaProjectASP.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CinemaProjectASP.Controllers
+{
+    public class KontoController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public KontoController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login() => View(new Login());
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login login)
+        {
+            if (!ModelState.IsValid) return View(login);
+
+            var user = await _userManager.FindByEmailAsync(login.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, login.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Filmy");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Please, try again!";
+                return View(login);
+            }
+
+            TempData["Error"] = "Wrong credentials. Please, try again!";
+            return View(login);
+        }
+
+    }
+}
---
+++ CinemaProjectASP/ViewModels/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaProjectASP.ViewModels
+{
+    public class Login
+    {
+        [Display(Name = "Adres e-mail")]
+        [Required(ErrorMessage = "Adres e-mail jest wymagany")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
