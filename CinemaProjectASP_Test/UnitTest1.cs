20be3fac1899f50b7e5bb855bcacadbe4009762e
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-28 19:16:56 +0100

    test

--- CinemaProjectASP_Test/UnitTest1.cs
+++ CinemaProjectASP_Test/UnitTest1.cs
@@ -1,15 +1,45 @@
 using System;
 using Xunit;
+using CinemaProjectASP.Models;
+using CinemaProjectASP.Controllers;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CinemaProjectASP.Data.Base;
 
-namespace CinemaProjectASP_Test
+namespace CinemaProjectASP
 {
     public class UnitTest1
     {
         [Fact]
-        public void TestGet()
+        public void GetFilmTest()
         {
-            
-
+            EntityBaseRepository repository = new EntityBaseRepository();
+            repository.AddAsync(new Film() { Nazwa = "TEST" });
+            repository.AddAsync(new Film() { Opis = "TEST" });
+            ApiAktorzyController controllerTest = new ApiAktorzyController(repository);
+            IList<Film> filmy = controllerTest.nowyFilm();
+            Assert.Equal(2, filmy.Count);
+        }
+        [Fact]
+        public void AddFilmTest()
+        {
+            EntityBaseRepository repository = new EntityBaseRepository();
+            repository.AddAsync(new Film() { Nazwa = "TEST" });
+            repository.AddAsync(new Film() { Opis = "TEST" });
+            ApiAktorzyController controllerTest = new ApiAktorzyController(repository);
+            Film newFilm = new Film() {Id = 50, Nazwa = "NEW" };
+            ActionResult<Film> actionResult = controllerTest.nowyFilm(nowyFilm);
+            Assert.Equal("NEW", repository.FindFilm(3).Name);
+        }
+        [Fact]
+        public void FindFilmId()
+        {
+            EntityBaseRepository repository = new EntityBaseRepository();
+            repository.AddAsync(new Film() { Id = 40, Nazwa = "TEST" });
+            repository.AddAsync(new Film() { Opis = "TEST" });
+            ApiAktorzyController controllerTest = new ApiAktorzyController(repository);
+            var test = repository.FindFilm(1);
+            Assert.Equal("TEST", test.Name);
         }
     }
 }
