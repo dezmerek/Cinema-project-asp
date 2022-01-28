177f6b55a68a0bf8f69b91b7acaeb496ffc909e9
Mateusz Szymański <mszymanski027@gmail.com>
2022-01-28 15:35:53 +0100

    api

---
+++ CinemaProjectASP/Controllers/ApiFilmyControllerr.cs
@@ -0,0 +1,107 @@
+﻿using CinemaProjectASP.Data;
+using CinemaProjectASP.Data.Services;
+using CinemaProjectASP.Data.Static;
+using CinemaProjectASP.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaProjectASP.Controllers
+{
+    [ApiController]
+    [Route("api/filmy")]
+    public class ApiAktorzyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ApiAktorzyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Film>>> GetFilm()
+        {
+            return await _context.Filmy.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Film>> GetFilm(int id)
+        {
+            var film = await _context.Filmy.FindAsync(id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return film;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFilm(int id, Film film)
+        {
+            if (id != film.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(film).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Film>> PostBook(Film film)
+        {
+
+            _context.Filmy.Add(film);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFilm", new { id = film.Id }, film);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFilm(int id)
+        {
+            var film = await _context.Filmy.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            _context.Filmy.Remove(film);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FilmExists(int id)
+        {
+            return _context.Filmy.Any(e => e.Id == id);
+        }
+    }
+}
